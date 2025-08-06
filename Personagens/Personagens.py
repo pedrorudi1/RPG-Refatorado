582d49c604d3127120bb8b28f5f5566e9bcf8e64
pedrorudi1 <pedro_rudi@hotmail.com>
2025-08-06 09:53:31 -0300

    Add initial implementation of Hero and Personagens classes

---
+++ Personagens/Personagens.py
@@ -0,0 +1,14 @@
+class Personagens:
+    def __init__(self, classe, name, HP, MP, strenght, magic, vitality, spirit, agility, luck):
+        self.classe = classe
+        self.name = name
+        self.HP = HP
+        self.MP = MP
+        self.strenght = strenght
+        self.magic = magic
+        self.vitality = vitality
+        self.spirit = spirit
+        self.agility = agility
+        self.luck = luck
+
+        
