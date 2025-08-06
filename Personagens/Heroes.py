91f1c475d8cd14db17f49f3be02e21362ce0dd46
pedrorudi1 <pedro_rudi@hotmail.com>
2025-08-06 17:41:06 -0300

    Refactor Personagens class: merge implementation into Heroes.py and enhance methods for equipping items and using skills

--- Personagens/Heroes.py
+++ Personagens/Heroes.py
@@ -1,5 +1,52 @@
-from Personagens import Personagens
 import random
+
+class Personagens:
+    MAX_HP = 9999
+    MAX_MP = 999
+    MAX_STATS = 255
+
+    def __init__(self, classe, name, HP, MP, strenght, magic, vitality, spirit, agility, luck):
+        self.classe = classe
+        self.name = name
+        self.HP = HP
+        self.MP = MP
+        self.strenght = strenght
+        self.magic = magic
+        self.vitality = vitality
+        self.spirit = spirit
+        self.agility = agility
+        self.luck = luck
+
+    def equip_weapon(self, weapon):
+        self.strenght += weapon.strenght
+        self.magic += weapon.magic
+        self.agility += weapon.agility
+        self.luck += weapon.luck
+    
+    def equip_armor(self, armor):
+        self.HP += armor.HP
+        self.MP += armor.MP
+        self.vitality += armor.vitality
+        self.spirit += armor.spirit
+        self.agility += armor.agility
+        self.luck += armor.luck
+
+    def equip_accessory(self, accessory):
+        self.HP += accessory.HP
+        self.MP += accessory.MP
+        self.strenght += accessory.strenght
+        self.magic += accessory.magic
+        self.vitality += accessory.vitality
+        self.spirit += accessory.spirit
+        self.agility += accessory.agility
+        self.luck += accessory.luck
+
+    def use_skill(self, skill):
+        self.MP -= skill.MP_cost
+
+    def use_magic(self, magic):
+        self.MP -= magic.MP_cost
+
 
 class Hero(Personagens):
     
