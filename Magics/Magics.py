85110cfbfd7ebdbb74e6a9ba252f49cfd013d83a
pedrorudi1 <pedro_rudi@hotmail.com>
2025-09-03 20:33:31 -0300

    Add magic classes for attack, healing, support, buff, debuff, and summoning

--- Magics/Magics.py
+++ Magics/Magics.py
@@ -1,2 +1,46 @@
-class Elements:
-    pass
+class AttackMagic:
+    def __init__(self, name, strength, target, element):
+        self.name = name
+        self.strength = strength
+        self.target = target
+        self.element = element
+
+
+class HealMagic:
+    def __init__(self, name, strength, target):
+        self.name = name
+        self.strength = strength
+        self.target = target
+
+
+class SupportMagic:
+    def __init__(self, name, effect, target):
+        self.name = name
+        self.effect = effect
+        self.target = target
+
+
+class BuffMagic:
+    def __init__(self, name, attribute, increase, duration):
+        self.name = name
+        self.attribute = attribute
+        self.increase = increase
+        self.duration = duration
+
+
+class DebuffMagic:
+    def __init__(self, name, attribute, decrease, duration):
+        self.name = name
+        self.attribute = attribute
+        self.decrease = decrease
+        self.duration = duration
+
+
+class SummonMagic:
+    def __init__(self, name, creature, strength, element):
+        self.name = name
+        self.creature = creature
+        self.strenght = strength
+        self.element = element
+
+
